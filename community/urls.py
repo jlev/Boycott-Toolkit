cbf2a5d4268f3ca47b1e45b9e27a5d8fe98d516a
Josh Levinger <jlev@dhcp-45-142.media.mit.edu>
2010-02-17 19:34:30 -0500

    wrote own auth views

--- community/urls.py
+++ community/urls.py
@@ -1,7 +1,12 @@
-from django.conf.urls.defaults import patterns
+from django.conf.urls.defaults import *
 
 urlpatterns = patterns('community.views',
-    (r'user/$','user_view_all'),
-    (r'user/(?P<username>[\w-]+)/$','user_view'),
-    (r'campaign/(?P<slug>[\w-]+)/$','campaign_view'),
+    url(r'login/$','login_view',name='auth_login'), #so facebookconnect can find the right view
+    url(r'logout/$','logout_view',name='auth_logout'),
+    url(r'change_password/$','change_password_view',name='auth_password_change'),
+    url(r'register/$','register_view'),
+    url(r'user/$','user_view_all'),
+    url(r'user/(?P<username>[\w-]+)/$','user_view'),
+    url(r'user/(?P<username>[\w-]+)/campaigns/$','user_campaign_view'),
+    url(r'user/(?P<username>[\w-]+)/cart/$','user_cart_view'),
 )
--- community/views.py
+++ community/views.py
@@ -1,12 +1,78 @@
-from django.http import HttpResponse,HttpResponseNotFound
+from django.http import HttpResponse,HttpResponseNotFound,HttpResponseRedirect
 from django.template import RequestContext
 from django.shortcuts import render_to_response,get_object_or_404,get_list_or_404
+from django.core.urlresolvers import reverse
+
+from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.models import Site
 
 from django.contrib.auth.models import User
 
+from community.forms import RegistrationForm
+from django.contrib.auth.forms import AuthenticationForm,PasswordChangeForm
+from django.views.decorators.cache import never_cache
+
+@never_cache
+def login_view(request):
+    "Displays the login form and handles the login action."
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            if request.session.test_cookie_worked():
+                request.session.delete_test_cookie()
+            return HttpResponseRedirect("/")
+        else:
+            print "Invalid Login"
+    else:
+        form = AuthenticationForm(request)
+    request.session.set_test_cookie()
+    return render_to_response("registration/login.html", {'form': form}, context_instance=RequestContext(request))
+
+def logout_view(request):
+    logout(request)
+    #clear cache?
+    return HttpResponseRedirect("/")
+    
+@login_required
+def change_password_view(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse("login_view"))
+    else:
+        form = PasswordChangeForm(request.user)
+    return render_to_response(template_name, {
+        'form': form,
+    }, context_instance=RequestContext(request))
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            opts = {}
+            opts['use_https'] = request.is_secure()
+            opts['email_template_name'] = 'registration/registration_email.txt'
+            form.save(**opts)
+            #login immediately
+            user = User.objects.get(username=form.cleaned_data['username'])
+            #supposed to use authenticate() here to set backend, but that seems to conflict with facebook connect
+            user.backend='django.contrib.auth.backends.ModelBackend'
+            login(request, user)
+            return HttpResponseRedirect('/')
+    else:
+        form = RegistrationForm()
+    
+    return render_to_response('registration/registration_form.html',
+                              {'form': form},
+                              RequestContext(request))
+
 def xd_receiver(request):
-    return render_to_response('registration/xd_receiver.html')
+    '''For Facebook login'''
+    return render_to_response('facebook/xd_receiver.html')
 
 def user_view_all(request):
     u = User.objects.all()
