42e649bdabab943473730f5ddb6b3aeea04d8ff8
Josh Levinger <jlev@dhcp-45-142.media.mit.edu>
2010-02-17 19:34:14 -0500

    more descriptive related_name

--- community/models.py
+++ community/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-from target.models import Product,Company
+from target.models import Product,Company,Campaign
 from django_stdimage import StdImageField
 
 class UserProfile(models.Model):
@@ -8,10 +8,11 @@ class UserProfile(models.Model):
     image = StdImageField(upload_to="uploads/users",blank=True,size=(400,400),thumbnail_size=(100,100))
     description = models.CharField(max_length=50)
     location = models.ForeignKey('geography.Location',null=True,blank=True)
-    buy = models.ManyToManyField(Product,related_name="buy",help_text="Products I buy",blank=True,null=True)
-    dontbuy = models.ManyToManyField(Product,related_name="dontbuy",help_text="Products I don't buy",blank=True,null=True)
-    supports = models.ManyToManyField(Company,related_name="supports",help_text="Companies I support",blank=True,null=True)
-    opposes = models.ManyToManyField(Company,related_name="opposes",help_text="Companies I oppose",blank=True,null=True)
+    campaigns = models.ManyToManyField(Campaign,related_name="users_joined_campaign",help_text="Campaigns I have joined",blank=True,null=True)
+    buy = models.ManyToManyField(Product,related_name="users_buy",help_text="Products I buy",blank=True,null=True)
+    dontbuy = models.ManyToManyField(Product,related_name="users_dontbuy",help_text="Products I don't buy",blank=True,null=True)
+    supports = models.ManyToManyField(Company,related_name="users_support",help_text="Companies I support",blank=True,null=True)
+    opposes = models.ManyToManyField(Company,related_name="users_oppose",help_text="Companies I oppose",blank=True,null=True)
     @models.permalink
     def get_absolute_url(self):
         return ('community.views.user_view', [self.user.username])
