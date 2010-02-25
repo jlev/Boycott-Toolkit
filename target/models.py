e815221b38b45a0fb6204c9280dd068b3810d147
Josh Levinger <jlev@jlev.local>
2010-02-25 02:40:59 -0500

    markdown help text

--- target/models.py
+++ target/models.py
@@ -8,7 +8,8 @@ from tagging.utils import parse_tag_input
 
 class TargetBase(models.Model):
     name = models.CharField('Name',max_length=200)
-    description = models.TextField(help_text="Can contain formatting in <a href='http://en.wikipedia.org/wiki/Markdown#Syntax_examples' target='_blank'>Markdown syntax</a>",blank=True,null=True)
+    description = models.TextField(help_text='''Can contain formatting in
+        <a href=http://en.wikipedia.org/wiki/Markdown#Syntax_examples target=_blank>Markdown syntax</a>''',blank=True,null=True)
     tags = tagging.fields.TagField()
     
     #these are required, but need to be null=true so that they can pass validation
