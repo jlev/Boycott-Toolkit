74c0fd9b57d526b52681961cac297b12c08a6ee2
Josh Levinger <jlev@dhcp-45-142.media.mit.edu>
2010-03-04 17:30:48 -0500

    properly redirect to next url on login

--- community/views.py
+++ community/views.py
@@ -17,13 +17,18 @@ from django.views.decorators.cache import never_cache
 @never_cache
 def login_view(request):
     "Displays the login form and handles the login action."
+    redirect_to = request.REQUEST.get('next')
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
+            # Light security check -- make sure redirect_to isn't garbage.
+            if not redirect_to or '//' in redirect_to or ' ' in redirect_to:
+                redirect_to = settings.LOGIN_REDIRECT_URL
+            from django.contrib.auth import login
             login(request, form.get_user())
             if request.session.test_cookie_worked():
                 request.session.delete_test_cookie()
-            return HttpResponseRedirect("/")
+            return HttpResponseRedirect(redirect_to)
         else:
             print "Invalid Login"
     else:
