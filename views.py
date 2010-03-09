9b2696de3b0f3537e3d17d4d8e0491f9cecbc703
Josh Levinger <jlev@dhcp-45-142.media.mit.edu>
2010-03-09 17:25:28 -0500

    move frontpage_auth to user_single. better frontpage

--- views.py
+++ views.py
@@ -1,54 +1,28 @@
 from django.http import HttpResponse,HttpResponseNotFound
 from django.template import RequestContext
-from django.shortcuts import render_to_response,get_object_or_404,redirect,Http404
+from django.shortcuts import render_to_response,get_object_or_404,redirect
 from django.views.decorators.cache import cache_control
 
 from django.contrib.auth.models import User
-from target.models import Campaign,Product,Company
+from target.models import Campaign,Product,Company,CompanyAction,ProductAction
 from geography.models import Location
 
-
-@cache_control(must_revalidate=True) #so the view refreshes when we logout
 def frontpage_view(request,message=None):
-    if request.user.is_anonymous():
-        campaigns = Campaign.objects.filter(highlight=True)
-        num_campaigns = Campaign.objects.count()
-        products = Product.objects.all()[:12]
-        num_users = User.objects.count()
-        #if not message:
-        #    message = "Welcome to the Boycott Toolkit"
-        return render_to_response('frontpage_noauth.html',
-            {'message':message,
-            'campaigns':campaigns,
-            'num_campaigns':num_campaigns,
-            'products':products},
-            context_instance = RequestContext(request))
-    else:
-        my_profile = request.user.profile
-        my_campaigns = my_profile.campaigns.all()
-        my_companies_support = my_profile.supports.all()
-        my_companies_oppose = my_profile.opposes.all()
-        my_products_buy = my_profile.buy.all()
-        my_products_dontbuy = my_profile.dontbuy.all()
-        #if not message:
-        #    message = 'Welcome back Boycotter!'
-        return render_to_response('frontpage_auth.html',
-            {'message':message,
-            'user':request.user,
-            'campaigns':my_campaigns,
-            'companies_support':my_companies_support,
-            'companies_oppose':my_companies_oppose,
-            'products_buy':my_products_buy,
-            'products_dontbuy':my_products_dontbuy},
-            context_instance = RequestContext(request))
+    campaigns = Campaign.objects.filter(highlight=True)
+    product_actions = ProductAction.objects.filter(campaign__in=campaigns)
+    company_actions = CompanyAction.objects.filter(campaign__in=campaigns)
+    
+    return render_to_response('frontpage.html',
+        {'message':message,
+        'campaigns':campaigns,
+        'product_actions':product_actions,
+        'company_actions':company_actions},
+        context_instance = RequestContext(request))
         
 def highlight_campaign_view(request,slug):
-    campaign = get_object_or_404(Campaign,slug=slug)
-    print campaign
+    campaign = get_object_or_404(Campaign,slug=slug) #raises 404 if none found
     if campaign.highlight:
         return redirect(campaign)
-    else:
-        raise Http404
 
 def search_view(request):
     '''The non-ajax search view'''
