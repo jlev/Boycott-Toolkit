ad5d380a6712ed28b6d3294d467e684f7abbb8fd
Josh Levinger <jlev@dhcp-45-142.media.mit.edu>
2010-03-12 18:18:48 -0500

    proxy

--- views.py
+++ views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse,HttpResponseNotFound
 from django.template import RequestContext
 from django.shortcuts import render_to_response,get_object_or_404,redirect
 from django.views.decorators.cache import cache_control
+import urllib2
 
 from django.contrib.auth.models import User
 from target.models import Campaign,Product,Company,CompanyAction,ProductAction
@@ -34,4 +35,62 @@ def search_view(request):
     return render_to_response('search.html',
         {'query':query,'results':results},
         context_instance = RequestContext(request))
-        
+
+
+def proxy(request,theURL):
+    """This is a blind proxy that we use to get around browser
+    restrictions that prevent the Javascript from loading pages not on the
+    same server as the Javascript.  This has several problems: it's less
+    efficient, it might break some sites, and it's a security risk because
+    people can use this proxy to browse the web and possibly do bad stuff
+    with it.  It only loads pages via http and https, but it can load any
+    content type. It supports GET requests."""
+
+    # Designed to prevent Open Proxy type stuff.
+    allowedHosts = ['groundtruth.media.mit.edu',]
+
+    response = HttpResponse()
+
+    #fix URL percent encoding
+    url = theURL.replace('%2F','/')
+    url.replace('%3A',':')
+
+    if url == "":
+        url = "http://www.example.com"
+
+    try:        
+        host = url.split("/")[2]
+        if allowedHosts and not host in allowedHosts:
+            response.write("Status: 502 Bad Gateway\n")
+            response.write("Content-Type: text/plain\n")
+            response.write("This proxy does not allow you to access the location (%s).\n" % (host,))
+
+        elif url.startswith("http://") or url.startswith("https://"):
+#           if request.method == "POST":
+#               length = int(request.META["CONTENT_LENGTH"])
+#               headers = {"Content-Type": request.META["CONTENT_TYPE"]}
+#               body = sys.stdin.read(length)
+#               ##fix
+#               r = urllib2.Request(url, body, headers)
+#               y = urllib2.urlopen(r)
+#           else:
+            y = urllib2.urlopen(url)
+
+            i = y.info()
+            if i.has_key("Content-Type"):
+                response.write("Content-Type: %s" % (i["Content-Type"]))
+            else:
+                response.write("Content-Type: text/plain\n")
+            response.write(y.read())
+            y.close()
+        else:
+            response.write("Content-Type: text/plain\n")
+            response.write("Illegal request.\n")
+
+    except Exception, E:
+        response.write("Status: 500 Unexpected Error\n")
+        response.write("Content-Type: text/plain\n")
+        response.write("Some unexpected error occurred. Error text was:\n")
+        response.write(str(E))
+
+    return response
