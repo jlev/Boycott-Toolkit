0e58aa01c8a47fe0458bc5f56bf4b0797f06057f
Josh Levinger <jlev@dhcp-45-142.media.mit.edu>
2010-03-17 18:26:11 -0400

    fix possible bug for requesting num fb friends for non fb user

--- community/models.py
+++ community/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
-from target.models import Product,Company,Campaign
 from django_stdimage import StdImageField
+from target.models import Product,Company,Campaign
+from facebookconnect.models import FacebookProfile
 
 class UserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
-    image = StdImageField(upload_to="uploads/users",blank=True,size=(400,400),thumbnail_size=(100,100))
-    description = models.CharField(max_length=50)
+    image = StdImageField(upload_to="uploads/users",blank=True,null=True,size=(400,400),thumbnail_size=(100,100))
+    #TODO: connect FacebookProfile post_save signal to cache avatar image
+    description = models.TextField(help_text="Tell us about yourself.",blank=True,null=True)
     campaigns = models.ManyToManyField(Campaign,related_name="users_joined_campaign",help_text="Campaigns I have joined",blank=True,null=True)
+    
     
     @models.permalink
     def get_absolute_url(self):
@@ -15,4 +18,14 @@ class UserProfile(models.Model):
         
     def __unicode__(self):
         return self.user.__unicode__()
+        
+    def get_num_fb_friends(self):
+        '''Get the number of friends'''
+        try:
+            my_profile = FacebookProfile.objects.get(user=self.user)
+            my_friends = my_profile._FacebookProfile__get_facebook_friends()
+            return len(my_friends)
+        except FacebookProfile.DoesNotExist:
+            return None
+        
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
