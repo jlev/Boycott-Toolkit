245b31ac385208161264f6a283c39efd0e2d4f8c
Josh Levinger <jlev@wireless-25-106.media.mit.edu>
2010-05-05 18:12:45 -0400

    simplify company query

--- autocomplete/views.py
+++ autocomplete/views.py
@@ -23,8 +23,8 @@ def main_search_ajax(request):
     products_by_company = Product.objects.filter(company__name__icontains=query)
     
     companies = Company.objects.filter( Q(name__icontains=query) |
-                                        Q(product=products) |
-                                        Q(map__name__icontains=query)
+                                        #Q(product=products) |
+                                        #Q(map__name__icontains=query)
                                       ).distinct()
     locations = Company.objects.filter(address__icontains=query)
     stores = Store.objects.filter(Q(address__icontains=query)|Q(name__icontains=query))
